4331921de151aa637beea123df7e7f61fa796b50
NexusStar <petar.nikov@gmail.com>
2015-05-27 08:09:08 +0300

    Added Week10

---
+++ Week10/ng-ToDoApp/server.js
@@ -0,0 +1,19 @@
+ "use strict";
+
+//Express app
+var express = require ('express'),
+	app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+//rounte
+app.get('/', function(req,res){
+	res.render('index')
+});
+
+// listen for requests
+var server = app.listen(3000, 'localhost');
+server.on('listening', function() {
+    console.log('Server listen on port %s at %s', server.address().port, server.address().address);
+});
